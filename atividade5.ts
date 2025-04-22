68455882abf0b8c978905bb5997277c5e3da8869
Tauane souza <tauane.souza@aluno.senai.br>
2025-04-22 16:39:02 -0300

    enviando atividades 1,2,3,4,5,6,7,8,9,10,11

---
+++ atividade5.ts
@@ -0,0 +1,23 @@
+/*10 pontos
+5 - Leia um conjunto de 5 notas e mostre a maior, a menor e a média.
+Nome Aluno: Tauane V.S. Souza 
+*/
+
+var teclado = require('prompt-sync')();
+var maior = 0;
+var menor = 100;
+var somaNotas = 0;
+for (var i = 1; i <= 5; i++) {
+    var nota = parseFloat(teclado("Digite a nota ".concat(i, ": ")));
+    somaNotas += nota;
+    if (nota > maior) {
+        maior = nota;
+    }
+    if (nota < menor) {
+        menor = nota;
+    }
+}
+var media = somaNotas / 5;
+console.log("Maior nota: ".concat(maior));
+console.log("Menor nota: ".concat(menor));
+console.log("M\u00E9dia das notas: ".concat(media.toFixed(2)));
---
+++ atividade6.ts
@@ -0,0 +1,23 @@
+/*10 pontos
+6 - Leia 10 números inteiros e informe, ao final:
+ A quantidade de números pares digitados e quais foram eles;
+ A quantidade de números ímpares digitados e quais foram eles.
+Nome aluno: Tauane V.S.Souza 
+*/
+
+var teclado = require('prompt-sync')();
+var contador = 1;
+var pares = 0;
+var impares = 0;
+while (contador <= 10) {
+    var numero = parseInt(teclado("Digite o ".concat(contador, "\u00BA n\u00FAmero inteiro: ")));
+    if (numero % 2 === 0) {
+        pares++;
+    }
+    else {
+        impares++;
+    }
+    contador++;
+}
+console.log("Quantidade de n\u00FAmeros pares: ".concat(pares));
+console.log("Quantidade de n\u00FAmeros \u00EDmpares: ".concat(impares));
